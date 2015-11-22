ffde6e7f470eccf90fe4def55d6c96fa150507b9
Kathryn Beaty <kabeaty@gmail.com>
2015-11-22 13:44:08 -0600

    Add a README with info on data and running the script; add empty codebook

---
+++ README.md
@@ -0,0 +1,19 @@
+## The data
+A description of the data analyzed by the script in this repo can be found here:
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+
+The data was collected from accelerometers from the Samsung Galaxy S smartphone.
+
+
+## Getting a tidy data set from the data
+The run_analysis.R script found in this repo can be used to return a tidy data set with the average of six of the accelerometer feature vector measurements for each of the six activities and each of the thirty training and test subjects.
+
+The activities are described by the variables STANDING, WALKING, LAYING, WALKING_DOWNSTAIRS, WALKING_UPSTAIRS, and SITTING. The subjects are identified by their numbers from 1 to 30.
+
+The six measurements that are averaged by subject and activity are those that measure the mean of the X, Y, and Z directions for body acceleration, and the standard deviation for the X, Y, and Z directions for body acceleration.
+
+This script contains two functions. The first function, get_dataset, returns a data frame with 8 columns for Subject, Activity, and each of the six measurements detailed above. The second function, get_averages, takes that data frame returned by get_dataset and loops over it, returning the average for each measurement for each unique combination of subject and activity. This function also returns a data frame with 8 columns for Subject, Activity, and the associated average of each of the six measurements. This data frame has 180 rows, one row for each of the six activities for each of the 30 training and test subjects.
+
+
+## Running the script
+Before running the script to get the tidy data set, you'll need to have the Samsung data loaded into your working directory. Once the data is loaded in, the run_analysis.R script should be run in the working directory to return the tidy data set.
--- run_analysis.R
+++ run_analysis.R
@@ -1,58 +1,61 @@
-# Read tables for training set and test set
-training_set <- read.table("./UCI HAR Dataset/train/X_train.txt")
-test_set <- read.table("./UCI HAR Dataset/test/X_test.txt")
+get_dataset <- function() {
+  # Read tables for training set and test set
+  training_set <- read.table("./UCI HAR Dataset/train/X_train.txt")
+  test_set <- read.table("./UCI HAR Dataset/test/X_test.txt")
 
-# Read tables for training labels and test labels
-train_labels <- read.table("./UCI HAR Dataset/train/y_train.txt")
-test_labels <- read.table("./UCI HAR Dataset/test/y_test.txt")
+  # Read tables for training labels and test labels
+  train_labels <- read.table("./UCI HAR Dataset/train/y_train.txt")
+  test_labels <- read.table("./UCI HAR Dataset/test/y_test.txt")
 
-# Read table for activity labels
-activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+  # Read table for activity labels
+  activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
-# Join training set and test set vertically
-joined_set <- rbind(training_set, test_set)
+  # Join training set and test set vertically
+  joined_set <- rbind(training_set, test_set)
 
-# Extract only mean and standard deviation measurements
-joined_set <- joined_set[1:6]
+  # Extract only mean and standard deviation measurements
+  joined_set <- joined_set[1:6]
 
-# Join the training labels and test labels vertically
-joined_labels <- rbind(train_labels, test_labels)
+  # Join the training labels and test labels vertically
+  joined_labels <- rbind(train_labels, test_labels)
 
-# Add numbers column to both in prep for merge
-joined_set$numbers <- rownames(joined_set)
-joined_labels$numbers <- rownames(joined_labels)
+  # Add numbers column to both in prep for merge
+  joined_set$numbers <- rownames(joined_set)
+  joined_labels$numbers <- rownames(joined_labels)
 
-# Merge labels with joined set
-merged_set_labels <- merge(joined_set, joined_labels, by.x="numbers", by.y="numbers")
+  # Merge labels with joined set
+  merged_set_labels <- merge(joined_set, joined_labels, by.x="numbers", by.y="numbers")
 
-# Add in activity labels to match up with activity numbers
-merged_set_labels <- merge(merged_set_labels, activity_labels, by.x="V1.y", by.y="V1")
+  # Add in activity labels to match up with activity numbers
+  merged_set_labels <- merge(merged_set_labels, activity_labels, by.x="V1.y", by.y="V1")
 
-# Rearrange order of columns with new activity column first
-merged_set_labels <- merged_set_labels[c(2,9,3,4,5,6,7,8)]
+  # Rearrange order of columns with new activity column first
+  merged_set_labels <- merged_set_labels[c(2,9,3,4,5,6,7,8)]
 
-# Rename columns more descriptively
-names(merged_set_labels)[names(merged_set_labels)=="V2.y"] <- "Activity"
-names(merged_set_labels)[names(merged_set_labels)=="V1.x"] <- "tBodyAccMeanX"
-names(merged_set_labels)[names(merged_set_labels)=="V2.x"] <- "tBodyAccMeanY"
-names(merged_set_labels)[names(merged_set_labels)=="V3"] <- "tBodyAccMeanZ"
-names(merged_set_labels)[names(merged_set_labels)=="V4"] <- "tBodyAccStdX"
-names(merged_set_labels)[names(merged_set_labels)=="V5"] <- "tBodyAccStdY"
-names(merged_set_labels)[names(merged_set_labels)=="V6"] <- "tBodyAccStdZ"
+  # Rename columns more descriptively
+  names(merged_set_labels)[names(merged_set_labels)=="V2.y"] <- "Activity"
+  names(merged_set_labels)[names(merged_set_labels)=="V1.x"] <- "tBodyAccMeanX"
+  names(merged_set_labels)[names(merged_set_labels)=="V2.x"] <- "tBodyAccMeanY"
+  names(merged_set_labels)[names(merged_set_labels)=="V3"] <- "tBodyAccMeanZ"
+  names(merged_set_labels)[names(merged_set_labels)=="V4"] <- "tBodyAccStdX"
+  names(merged_set_labels)[names(merged_set_labels)=="V5"] <- "tBodyAccStdY"
+  names(merged_set_labels)[names(merged_set_labels)=="V6"] <- "tBodyAccStdZ"
 
-# Read tables for subject training and test files
-subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
-subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+  # Read tables for subject training and test files
+  subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+  subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 
-# merge subjects together, also by numbers column from row names
-merged_subjects <- rbind(subject_train, subject_test)
-merged_subjects$numbers <- rownames(merged_subjects)
-merge_w_subjects <- merge(merged_set_labels, merged_subjects, by.x="numbers", by.y="numbers")
-merge_w_subjects <- subset(merge_w_subjects, select=-numbers)
+  # merge subjects together, also by numbers column from row names
+  merged_subjects <- rbind(subject_train, subject_test)
+  merged_subjects$numbers <- rownames(merged_subjects)
+  merge_w_subjects <- merge(merged_set_labels, merged_subjects, by.x="numbers", by.y="numbers")
+  merge_w_subjects <- subset(merge_w_subjects, select=-numbers)
 
-# Rename subjects column and reorder
-names(merge_w_subjects)[names(merge_w_subjects)=="V1"] <- "Subject"
-merge_w_subjects <- merge_w_subjects[c(8,1,2,3,4,5,6,7)]
+  # Rename subjects column and reorder
+  names(merge_w_subjects)[names(merge_w_subjects)=="V1"] <- "Subject"
+  merge_w_subjects <- merge_w_subjects[c(8,1,2,3,4,5,6,7)]
+  merge_w_subjects
+}
 
 # Function for getting averages by subject and activity
 get_averages <- function(dataset) {
@@ -80,4 +83,4 @@ get_averages <- function(dataset) {
 }
 
 # Run function to return data frame with averages by subject and activity
-get_averages(merge_w_subjects)
+get_averages(get_dataset())
