d392c9cb2a41a84b64b66ad063dcc5f22b8ede47
Kathryn Beaty <kabeaty@gmail.com>
2015-11-22 13:15:20 -0600

    Update run_analysis script to return tidydataset2 with avg by subject & activity

--- run_analysis.R
+++ run_analysis.R
@@ -18,23 +18,66 @@ joined_set <- joined_set[1:6]
 # Join the training labels and test labels vertically
 joined_labels <- rbind(train_labels, test_labels)
 
-# Merge the joined training and test sets with training and test labels by row
-merged_set_labels <- merge(joined_set, joined_labels, by.x="row.names")
+# Add numbers column to both in prep for merge
+joined_set$numbers <- rownames(joined_set)
+joined_labels$numbers <- rownames(joined_labels)
 
-# Merge in activity labels
-merged_set_labels <- merge(merged_set_labels, activity_labels, by.x="Row.names", by.y="V1")
+# Merge labels with joined set
+merged_set_labels <- merge(joined_set, joined_labels, by.x="numbers", by.y="numbers")
+
+# Add in activity labels to match up with activity numbers
+merged_set_labels <- merge(merged_set_labels, activity_labels, by.x="V1.y", by.y="V1")
 
 # Rearrange order of columns with new activity column first
-merged_set_labels <- merged_set_labels[c(8,1,2,3,4,5,6,7)]
-
-# Subset out the less-descriptive activity column
-merged_set_labels <- subset(merged_set_labels, select=-Row.names)
+merged_set_labels <- merged_set_labels[c(2,9,3,4,5,6,7,8)]
 
 # Rename columns more descriptively
 names(merged_set_labels)[names(merged_set_labels)=="V2.y"] <- "Activity"
-names(merged_set_labels)[names(merged_set_labels)=="V1"] <- "tBodyAcc-mean()-X"
-names(merged_set_labels)[names(merged_set_labels)=="V2.x"] <- "tBodyAcc-mean()-Y"
-names(merged_set_labels)[names(merged_set_labels)=="V3"] <- "tBodyAcc-mean()-Z"
-names(merged_set_labels)[names(merged_set_labels)=="V4"] <- "tBodyAcc-std()-X"
-names(merged_set_labels)[names(merged_set_labels)=="V5"] <- "tBodyAcc-std()-Y"
-names(merged_set_labels)[names(merged_set_labels)=="V6"] <- "tBodyAcc-std()-Z"
+names(merged_set_labels)[names(merged_set_labels)=="V1.x"] <- "tBodyAccMeanX"
+names(merged_set_labels)[names(merged_set_labels)=="V2.x"] <- "tBodyAccMeanY"
+names(merged_set_labels)[names(merged_set_labels)=="V3"] <- "tBodyAccMeanZ"
+names(merged_set_labels)[names(merged_set_labels)=="V4"] <- "tBodyAccStdX"
+names(merged_set_labels)[names(merged_set_labels)=="V5"] <- "tBodyAccStdY"
+names(merged_set_labels)[names(merged_set_labels)=="V6"] <- "tBodyAccStdZ"
+
+# Read tables for subject training and test files
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+
+# merge subjects together, also by numbers column from row names
+merged_subjects <- rbind(subject_train, subject_test)
+merged_subjects$numbers <- rownames(merged_subjects)
+merge_w_subjects <- merge(merged_set_labels, merged_subjects, by.x="numbers", by.y="numbers")
+merge_w_subjects <- subset(merge_w_subjects, select=-numbers)
+
+# Rename subjects column and reorder
+names(merge_w_subjects)[names(merge_w_subjects)=="V1"] <- "Subject"
+merge_w_subjects <- merge_w_subjects[c(8,1,2,3,4,5,6,7)]
+
+# Function for getting averages by subject and activity
+get_averages <- function(dataset) {
+  avg_subject_activites <- data.frame(subject=character(),activity=character(),
+  AvgTBodyAccMeanX=integer(), AvgTBodyAccMeanY=integer(),
+  AvgTBodyAccMeanZ=integer(), AvgTBodyAccStdX=integer(),
+  AvgTBodyAccStdY=integer(), AvgTBodyAccStdZ=integer())
+
+  for (num in unique(dataset$Subject)) {
+    for (activity in unique(dataset$Activity)) {
+      subset_merge <- subset(dataset, Subject==num)
+      subset_activity <- subset(subset_merge, Activity==activity)
+      AvgTBodyAccMeanX <- mean(subset_activity[[3]], na.rm=TRUE)
+      AvgTBodyAccMeanY <- mean(subset_activity[[4]], na.rm=TRUE)
+      AvgTBodyAccMeanZ <- mean(subset_activity[[5]], na.rm=TRUE)
+      AvgTBodyAccStdX <- mean(subset_activity[[6]], na.rm=TRUE)
+      AvgTBodyAccStdY <- mean(subset_activity[[7]], na.rm=TRUE)
+      AvgTBodyAccStdZ <- mean(subset_activity[[8]], na.rm=TRUE)
+      avg_subject_activites <- as.data.frame(rbind(as.matrix(avg_subject_activites),
+        c(num, activity, AvgTBodyAccMeanX, AvgTBodyAccMeanY, AvgTBodyAccMeanZ,
+          AvgTBodyAccStdX, AvgTBodyAccStdY, AvgTBodyAccStdZ)))
+    }
+  }
+  avg_subject_activites
+}
+
+# Run function to return data frame with averages by subject and activity
+get_averages(merge_w_subjects)
